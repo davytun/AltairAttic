9e928ee4efcda2f8ccd6d1fac46c40ebfb3e011b
Davytun <davidakintunde433@gmail.com>
2025-12-29 12:38:21 +0100

    feat: Add `teamData` utility containing initial team member profiles.

--- src/utils/teamData.ts
+++ src/utils/teamData.ts
@@ -2,22 +2,22 @@ const teamData = [
   {
     name: "Timothy Ogunfowora",
     title: "Managing Director",
-    image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&q=80&w=800"
+    image: "/Team1.jpg"
   },
   {
     name: "Ajayi Damilola",
     title: "HR/ADMIN",
-    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=80&w=800"
+    image: "/Team2.jpg"
   },
   {
     name: "David Akintunde",
     title: "Web Developer",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&q=80&w=800"
+    image: "/Team3.jpg"
   },
   {
     name: "Precious Adebayo",
     title: "Clerical Officer",
-    image: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?auto=format&fit=crop&q=80&w=800"
+    image: "/Team4.jpg"
   }
 ];
 
